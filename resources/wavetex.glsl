584bf2b1d47f29189c06c7d3c1e9ec3a04ae93c0
Bob-Loth <bob94loth@gmail.com>
2021-12-06 17:35:26 -0800

    DEMO READY, dissipating waves

--- resources/wavetex.glsl
+++ resources/wavetex.glsl
@@ -36,7 +36,7 @@ out vec3 EPos;
 void main() {
 
   vec4 vPosition;
-  float lifespan = 3.0f;
+  float lifespan = 4.0f;
   float remaining1 = max(0.0f, lifespan - (time - splashPosition1.a));
   float remaining2 = max(0.0f, lifespan - (time - splashPosition2.a));
   float remaining3 = max(0.0f, lifespan - (time - splashPosition3.a));
@@ -56,11 +56,11 @@ void main() {
   float distance4 = distance(EPos, splashPosition4.xyz);
   float distance5 = distance(EPos, splashPosition5.xyz);
 
-  float propagationAdjust1 = sin(pi * remaining1/lifespan);
-  float propagationAdjust2 = sin(pi * remaining2/lifespan);
-  float propagationAdjust3 = sin(pi * remaining3/lifespan);
-  float propagationAdjust4 = sin(pi * remaining4/lifespan);
-  float propagationAdjust5 = sin(pi * remaining5/lifespan);
+  float propagationAdjust1 = 1/distance1 * sin(pi * remaining1/lifespan);
+  float propagationAdjust2 = 1/distance2 * sin(pi * remaining2/lifespan);
+  float propagationAdjust3 = 1/distance3 * sin(pi * remaining3/lifespan);
+  float propagationAdjust4 = 1/distance4 * sin(pi * remaining4/lifespan);
+  float propagationAdjust5 = 1/distance5 * sin(pi * remaining5/lifespan);
   
   float wavelength = 2.0f;
   float offsetY1 = length(splashForce1) * sin(wavelength * distance1 * remaining1) * propagationAdjust1;
@@ -69,8 +69,8 @@ void main() {
   float offsetY4 = length(splashForce4) * sin(wavelength * distance4 * remaining4) * propagationAdjust4;
   float offsetY5 = length(splashForce5) * sin(wavelength * distance5 * remaining5) * propagationAdjust5;
   
-  gl_Position = P * V *M * vec4(vertPos.x, vertPos.y + offsetY1 + offsetY2 + offsetY3 + offsetY4 + offsetY5, vertPos.z, 1.0);
-  EPos = (M * vec4(vertPos.x,vertPos.y + offsetY1 + offsetY2 + offsetY3 + offsetY4 + offsetY5, vertPos.z ,1.0)).xyz;
+  gl_Position = P * V *M * vec4(vertPos.x, vertPos.y - (offsetY1 + offsetY2 + offsetY3 + offsetY4 + offsetY5), vertPos.z, 1.0);
+  EPos = (M * vec4(vertPos.x,vertPos.y - (offsetY1 + offsetY2 + offsetY3 + offsetY4 + offsetY5), vertPos.z ,1.0)).xyz;
   /* pass through the texture coordinates to be interpolated */
   vTexCoord = vertTex;
 }
